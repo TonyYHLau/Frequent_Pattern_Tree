d4dfe3578ceaa9e68ed4fb1ef7c42b230cda2396
TonyYHLau <lauyuenhoi@hotmail.com>
2017-11-09 13:03:46 +0800

    Update Mine_Frequent_Patterns.py

--- Mine_Frequent_Patterns.py
+++ Mine_Frequent_Patterns.py
@@ -284,6 +284,9 @@ def conditional_FPtree(paths):
 #####  Implementation  #####
 if __name__=='__main__':
     
+    print('Sorted frequent items: ', sorted_frequent_items(transactions,3))#print the sorted frequent items
+    print()
+    
     OrderedFI = list_OrderedFI(transactions,threshold=3) # Ordered frequent items in each transaction
     Atree=Tree()
     for i in range(len(OrderedFI)):
@@ -305,3 +308,92 @@ if __name__=='__main__':
     print('Print all frequent pattern combinations of the conditional tree:')
     for j in mine_frequent_patterns(BTree,threshold=3,suffix=[]):
         print(j) #print all frequent patterns of the conditional tree
+        
+''' The Output is like the following:
+
+Sorted frequent items:  [('f', 4), ('c', 4), ('a', 3), ('m', 3), ('p', 3), ('b', 3)]
+
+Tree:
+  <Node (root)>
+    <Node 'f' (4)>
+      <Node 'c' (3)>
+        <Node 'a' (3)>
+          <Node 'm' (3)>
+            <Node 'p' (2)>
+            <Node 'b' (1)>
+      <Node 'b' (1)>
+    <Node 'c' (1)>
+      <Node 'p' (1)>
+        <Node 'b' (1)>
+
+Linked node Routes:
+  'f'
+    <Node 'f' (4)>
+  'c'
+    <Node 'c' (3)>
+    <Node 'c' (1)>
+  'a'
+    <Node 'a' (3)>
+  'm'
+    <Node 'm' (3)>
+  'p'
+    <Node 'p' (2)>
+    <Node 'p' (1)>
+  'b'
+    <Node 'b' (1)>
+    <Node 'b' (1)>
+    <Node 'b' (1)>
+
+Print all frequent pattern combinations of the tree:
+(['f'], 4)
+(['c'], 4)
+(['f', 'c'], 3)
+(['a'], 3)
+(['f', 'a'], 3)
+(['c', 'a'], 3)
+(['f', 'c', 'a'], 3)
+(['m'], 3)
+(['f', 'm'], 3)
+(['c', 'm'], 3)
+(['f', 'c', 'm'], 3)
+(['a', 'm'], 3)
+(['f', 'a', 'm'], 3)
+(['c', 'a', 'm'], 3)
+(['f', 'c', 'a', 'm'], 3)
+(['p'], 3)
+(['c', 'p'], 3)
+(['b'], 3)
+
+Conditional tree: 
+Tree:
+  <Node (root)>
+    <Node 'f' (2)>
+      <Node 'c' (1)>
+        <Node 'a' (1)>
+          <Node 'm' (1)>
+            <Node 'b' (1)>
+      <Node 'b' (1)>
+    <Node 'c' (1)>
+      <Node 'p' (1)>
+        <Node 'b' (1)>
+
+Linked node Routes:
+  'f'
+    <Node 'f' (2)>
+  'c'
+    <Node 'c' (1)>
+    <Node 'c' (1)>
+  'a'
+    <Node 'a' (1)>
+  'm'
+    <Node 'm' (1)>
+  'b'
+    <Node 'b' (1)>
+    <Node 'b' (1)>
+    <Node 'b' (1)>
+  'p'
+    <Node 'p' (1)>
+
+Print all frequent pattern combinations of the conditional tree:
+(['b'], 3)
+'''
